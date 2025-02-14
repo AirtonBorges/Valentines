d5220f56b62262c2c63c01fe1bd47add97bda5f8
AirtonBorges <airtonborges2211@gmail.com>
2025-02-14 12:42:25 -0300

    fix: muito dificil de fazer aparecer as flores

--- src/index.ts
+++ src/index.ts
@@ -5,8 +5,8 @@ import './style.css';
 
 const listaShake = [
     { numero: 100, atingiu: false }
-    , { numero: 70, atingiu: false }
-    , { numero: 90, atingiu: false }
+    , { numero: 103, atingiu: false }
+    , { numero: 105, atingiu: false }
 ]
 
 let carregou = false;
@@ -20,7 +20,7 @@ const app = new Application(canvas);
 alert("Ei, balança um pouco o celular, que eu vou te dar um presente!");
 
 const detectDeviceOrientation = new DetectDeviceOrientation();
-detectDeviceOrientation.init((orientation: Orientation) => {
+detectDeviceOrientation.init(async (orientation: Orientation) => {
     const numero = listaShake.find(p => p.numero === Math.round(orientation.alpha));
     const antingiuTodos = listaShake.every(p => p.atingiu);
 
@@ -32,7 +32,7 @@ detectDeviceOrientation.init((orientation: Orientation) => {
 
     if (antingiuTodos && !carregou) {
         carregou = true;
-        app.load('https://prod.spline.design/XshLL8MCRiz3Wg-q/scene.splinecode');
+        await app.load('https://prod.spline.design/XshLL8MCRiz3Wg-q/scene.splinecode');
         alert("Te amo muito pqena, agora vc tem flores virtuais que duram pra sempre");
         titulo.innerText = "Para: Julia Mariana da Silva";
     }
