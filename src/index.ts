922caef0983c2d4ac052b780f55ea26994209e7d
AirtonBorges <airtonborges2211@gmail.com>
2025-02-14 02:12:26 -0300

    wip presente valentines

---
+++ src/index.ts
@@ -0,0 +1,39 @@
+import { Application } from '@splinetool/runtime';
+import { DetectDeviceOrientation, Orientation } from "detect-device-orientation";
+
+import './style.css';
+
+const listaShake = [
+    { numero: 100, atingiu: false }
+    , { numero: 70, atingiu: false }
+    , { numero: 90, atingiu: false }
+]
+
+let carregou = false;
+let mensagem = "Mais um pouco!"
+
+const titulo = document.getElementById('titulo') as HTMLHeadingElement;
+const canvas = document.getElementById('canvas3d') as HTMLCanvasElement;
+
+const app = new Application(canvas);
+
+alert("Ei, balança um pouco o celular, que eu vou te dar um presente!");
+
+const detectDeviceOrientation = new DetectDeviceOrientation();
+detectDeviceOrientation.init((orientation: Orientation) => {
+    const numero = listaShake.find(p => p.numero === Math.round(orientation.alpha));
+    const antingiuTodos = listaShake.every(p => p.atingiu);
+
+    if (numero && numero.atingiu === false) {
+        mensagem += "!";
+        titulo.innerText = mensagem
+        numero.atingiu = true;
+    }
+
+    if (antingiuTodos && !carregou) {
+        carregou = true;
+        app.load('https://prod.spline.design/XshLL8MCRiz3Wg-q/scene.splinecode');
+        alert("Te amo muito pqena, agora vc tem flores virtuais que duram pra sempre");
+        titulo.innerText = "Para: Julia Mariana da Silva";
+    }
+});
